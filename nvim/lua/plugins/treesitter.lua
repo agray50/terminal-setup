6c394145200a11f0546feb772258e91e8cd50395
agray50 <grayaodhan3@gmail.com>
2025-12-01 15:08:27 +0000

    Complete Dev Setup

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        ensure_installed = {
+          "bash",
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "typescript",
+          "json",
+          "lua",
+          "yaml",
+          "python",
+        },
+        highlight = { enable = true },
+        indent = { enable = false },
+      })
+    end
+  }
+}
