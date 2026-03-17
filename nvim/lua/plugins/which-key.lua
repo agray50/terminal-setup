47b548650c77026dd9fe8f426f381a59879292a9
agray50 <grayaodhan3@gmail.com>
2026-03-17 08:21:33 +0000

    Which-key configure

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,7 +2,19 @@ return {
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			spec = {
+				-- Leader groups
+				{ "<leader>f", group = "find (telescope)" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>h", group = "hunk (gitsigns)" },
+				{ "<leader>l", group = "lsp" },
+				{ "<leader>m", group = "format" },
+				{ "<leader>t", group = "toggle" },
+				-- gr* LSP builtins (Neovim 0.11)
+				{ "gr", group = "lsp" },
+			},
+		},
 		keys = {
 			{
 				"<leader>?",
